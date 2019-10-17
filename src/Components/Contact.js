8b4405f35121bea5d83b095b2d18df5ca9da956e
pierrekzo <pierrecasaux@gmail.com>
2019-10-17 16:22:59 +0200

    click sur status activé

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -16,4 +16,31 @@ function Contact(props) {
     );
 }
 
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online = false,
+        };
+    }
+    render() {
+        return (
+            <div className="Contact">
+                <img className="avatar" src={props.avatar} alt="avatar profile" />
+                <div>
+                    <h4 className="name">{props.name}</h4>
+                    <div className="status"
+                        onClick={event => {
+                            const newStatus = !this.state.online;
+                            this.setState({ online = newStatus });
+                        }}
+                    ><span className={props.online ? "status-online" : "status-offline"}> </span>
+                        <span className="status-text"> {props.online ? 'online' : 'offline'}</span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
 export default Contact;
